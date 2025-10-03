3806f48624a9216a0f254694bd4ee424f1bf9853
AumParekh07 <aumparekh2003@gmail.com>
2025-10-03 19:04:44 +0530

    nodemailer fix 2.1

--- university-be/src/helper/userHelper.ts
+++ university-be/src/helper/userHelper.ts
@@ -12,9 +12,9 @@ export const sendEmail = async (email: string, otp: string) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      // tls: {
-      //   rejectUnauthorized: false,
-      // },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
     const info = await transporter.sendMail({
       from: process.env.EMAIL_USER,
