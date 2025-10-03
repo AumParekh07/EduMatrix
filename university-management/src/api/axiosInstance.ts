1657337d9d3c6603c6b736a975df5324a708c70c
AumParekh07 <aumparekh2003@gmail.com>
2025-10-03 18:43:16 +0530

    Update API call timeout for nodemailer issue

--- university-management/src/api/axiosInstance.ts
+++ university-management/src/api/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { GetToken } from '../helper/getAuth';
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3007/api/";
 const axiosInstance = axios.create({
     baseURL: backendUrl,
-    timeout: 10000,
+    timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
     },
